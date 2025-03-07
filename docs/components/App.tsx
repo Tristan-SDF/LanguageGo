acbf5ff33ad2c0f8a1d903b28a620374c641b506
Tristan <tristan_green@brown.edu>
2025-03-07 13:50:54 -0500

    Backup Upload

--- docs/components/App.tsx
+++ docs/components/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LearnPage from "../components/pages/LearnPage";
-import HomePage from "../components/pages/HomePage";
-import PracticePage from "../components/pages/PracticePage";
-import LeaderBoard from "../components/pages/LeaderBoard";
+import LearnPage from "./pages/LearnPage";
+import HomePage from "./pages/HomePage";
+import PracticePage from "./pages/PracticePage";
+import LeaderBoard from "./pages/LeaderBoard";
 
 
 const App = () => {
